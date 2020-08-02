5e5ad55aebc5b3e19f3595a324c0c486bf3f45c9
Viktoria Kolesnik <67219664+VictoriaKolesnyk@users.noreply.github.com>
2020-08-02 13:42:41 +0300

    Update seassons.py

--- seassons.py
+++ seassons.py
@@ -1,13 +1,13 @@
 def season(a):
     if a == 1 or a == 2 or a == 12:
-        print("это зима")
+        return "это зима"
     elif a == 3 or a == 4 or a == 5:
-        print("это весна")
+        return "это весна"
     elif a == 6 or a == 7 or a == 8:
-        print("это лето")
+        return "это лето"
     elif a == 9 or a == 10 or a == 11:
-        print("это осень")
+        return "это осень"
     else:
-        print("Такого месяца нет")
+        return "Такого месяца нет"
 a = int(input('введите месяц года   '))
-season(a)
+print(season(a))
